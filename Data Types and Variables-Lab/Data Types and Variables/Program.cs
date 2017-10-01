9c07d1f4975e8faeb957f989298557c7452edd65
giggals <nikolaydddd@abv.bg>
2017-10-01 12:43:21 +0300

    Adding some exercise.

---
+++ Data Types and Variables-Lab/Data Types and Variables/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Types_and_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            int years = centuries * 100;
+            int days =(int) (years * 365.2422);
+            int hours = days * 24;
+            int minuts = hours * 60;
+
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minuts} minuts");
+
+
+
+
+
+        }
+    }
+}
---
+++ Data Types and Variables-Lab/Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactor_Volume_of_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lenght = 0;
+
+            Console.Write("Length: ");
+            lenght = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            double width = 0;
+            width = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            double height = 0;
+            height  = double.Parse(Console.ReadLine());
+            height  = (lenght * width * height ) / 3 ;
+            Console.WriteLine($"Pyramid Volume: {height:F2}");
+           
+        }
+    }
+}
