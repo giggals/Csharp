69fb9211106fbb148724dfe8bbe1c03bd55bc9f5
giggals <nikolaydddd@abv.bg>
2017-10-17 17:09:09 +0300

    Adding exercises.

---
+++ Dictionary - Exercises/Fix Emails/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fix_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
+            while (true)
+            {
+                string key = Console.ReadLine();
+
+                if (key == "stop")
+                {
+                    break;
+                }
+                else
+                {
+                    dict[key] = Console.ReadLine();
+                }
+                
+            }
+            foreach (KeyValuePair <string,string> kvp in dict)
+            {
+                if (!kvp.Value.EndsWith("us") && !kvp.Value.EndsWith("uk"))
+                {
+                    Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ Dictionary - Exercises/PhoneBook Upgrade/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Phonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var phonebook = new SortedDictionary<string, string>();
+            var comand = Console.ReadLine().Split(' ').ToArray();
+            while (comand[0] != "END")
+            {
+                if (comand[0] == "A")
+                    phonebook[comand[1]] = comand[2];
+                if (comand[1] == "S")
+                {
+                    if (phonebook.ContainsKey(comand[1]))
+                    {
+                        Console.WriteLine($"Contact {comand[1]} -> {phonebook[comand[1]]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {comand[1]} does not exist.");
+                    }
+                }
+                else if (comand[0] == "ListAll")
+                    foreach (var item in phonebook)
+                        Console.WriteLine($"{item.Key} -> {item.Value}");
+                comand = Console.ReadLine().Split(' ').ToArray();
+            }
+        }
+    }
+}
