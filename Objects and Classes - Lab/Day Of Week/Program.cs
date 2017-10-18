bab0c7e2af8303e495a78e87e2cea2db392c1bbe
giggals <nikolaydddd@abv.bg>
2017-10-18 21:28:40 +0300

    Adding Lab Exercises.

---
+++ Objects and Classes - Lab/Day Of Week/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects_and_Classes___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            DateTime myDate = DateTime.ParseExact(input, "d-M-yyyy", CultureInfo.InvariantCulture);
+
+            Console.WriteLine(myDate.DayOfWeek);
+
+        }
+    }
+}
---
+++ Objects and Classes - Lab/Randomize Words/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int randomizer = rnd.Next(input.Length);
+            }
+
+            Console.WriteLine(string.Join("\r\n",input));
+        }
+    }
+}
