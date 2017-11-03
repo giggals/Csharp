5ffbd75d9f7b8920db7522b6f67476722b33cf76
giggals <nikolaydddd@abv.bg>
2017-11-03 20:30:10 +0200

    Exam Prep 1.

---
+++ JustTesting/03. Rage Quit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _03.Rage_Quit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+
+            string pattern = @"([^0-9]+)(\d+)";
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (Match m in Regex.Matches(inputLine, pattern, RegexOptions.Multiline))
+            {
+                var word = m.Groups[1].Value.ToString().ToUpper();
+                var counter =int.Parse(m.Groups[2].Value);
+                
+
+                for (int i = 0; i < counter; i++)
+                {
+                    result.Append(word);
+                }
+            }
+            var uniqueSymbols = result.ToString().Distinct().Count();
+            Console.WriteLine($"Unique symbols used: {uniqueSymbols}");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ JustTesting/Softuni Coffee Orders/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int orders = int.Parse(Console.ReadLine());
+
+          
+            decimal price = 0;
+            decimal totalPrice = 0;
+
+            for (int i = 0; i < orders; i++)
+            {
+                decimal pricePerCapsule = decimal.Parse(Console.ReadLine());
+                DateTime date = DateTime.ParseExact(Console.ReadLine(), "d/M/yyyy", CultureInfo.InvariantCulture);
+                int daysInMonth = DateTime.DaysInMonth(date.Year, date.Month);
+                long capsuleCount = long.Parse(Console.ReadLine());
+
+                    price = (daysInMonth * capsuleCount) * pricePerCapsule;
+                    totalPrice += price;
+                    Console.WriteLine($"The price for the coffee is: ${price:f2}");
+            }
+            
+            Console.WriteLine($"Total: ${totalPrice:f2}");
+        }
+    }
+}
