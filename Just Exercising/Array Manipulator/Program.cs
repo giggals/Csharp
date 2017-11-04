112fc4582ac5d1de0a4590cd239c5a3e4471066f
giggals <nikolaydddd@abv.bg>
2017-11-04 22:13:18 +0200

    Exercises

---
+++ Just Exercising/Array Manipulator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Array_Manipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int picuresCount = int.Parse(Console.ReadLine());
+            int filterTime = int.Parse(Console.ReadLine());
+            int percentage = int.Parse(Console.ReadLine());
+            int uploadTime = int.Parse(Console.ReadLine());
+
+            decimal goodPhotos = picuresCount * percentage / 100;
+
+            decimal firstseconds = goodPhotos * filterTime;
+
+            decimal secondseconds = goodPhotos * uploadTime;
+
+            decimal totalTimeSeconds = firstseconds + secondseconds;
+            decimal minuts = totalTimeSeconds / 60;
+            decimal hours = minuts * 60;
+            decimal days = hours * 24;
+            var result = "days:hours:minuts:totalTimeSeconds";
+            DateTime date = DateTime.ParseExact(result, "d:HH:mm:ss", CultureInfo.InvariantCulture);
+
+            Console.WriteLine(date);
+
+        }
+    }
+}
---
+++ Just Exercising/Charity Marathon/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_23._10._2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            long runners = long.Parse(Console.ReadLine());
+            int numberOfLaps = int.Parse(Console.ReadLine());
+            int laplenth = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            decimal moneyPerKilometer = decimal.Parse(Console.ReadLine());
+
+            decimal moneyRaised = 0;
+
+            if (runners > capacity * days)
+            {
+                runners = capacity * days;
+            }
+
+            decimal km = (runners * numberOfLaps * laplenth ) / 1000;
+            moneyRaised = km * moneyPerKilometer;
+
+            Console.WriteLine($"Money raised: {moneyRaised:f2}");
+        }
+    }
+}
