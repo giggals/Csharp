e65a20515f302833e7b8a58337ad5e02ada86131
giggals <nikolaydddd@abv.bg>
2018-02-23 16:33:12 +0200

    Lab-Inheritance.sln

---
+++ Lab-Inheritance/1. Single Inheritance/RandomList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class RandomList : List<string>
+{
+    private Random rnd;
+
+
+    public string RandomString()
+    {
+
+        string result = null;
+        if (this.Count > 0)
+        {
+            var randomIndex = rnd.Next(0, this.Count - 1);
+            result = this[randomIndex];
+            this.RemoveAt(randomIndex);
+        }
+        return result;
+    }
+
+}
+
---
+++ Lab-Inheritance/1. Single Inheritance/StackOfStrings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class StackOfStrings
+{
+    private List<string> data = new List<string>();
+
+    public void Push(string item)
+    {
+        data.Add(item);
+    }
+
+    public string Pop()
+    {
+        string result = string.Empty;
+        if (!IsEmpty())
+        {
+            result = data[data.Count - 1];
+            data.RemoveAt(data.Count - 1);
+        }
+        return result;
+    }
+
+    public string Peek()
+    {
+        var result = string.Empty;
+        if (!IsEmpty())
+        {
+            result = data[data.Count - 1];
+            
+        }
+        return result;
+    }
+
+    public bool IsEmpty()
+    {
+        return data.Count == 0; 
+    }
+
+  
+}
+
