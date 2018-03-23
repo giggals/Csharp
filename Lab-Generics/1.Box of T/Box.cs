363b0d61293a426731afc827568aefa30b4ec8ba
giggals <nikolaydddd@abv.bg>
2018-03-23 18:19:57 +0200

    Lab-Generics.sln

---
+++ Lab-Generics/1.Box of T/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+public class Box<T>
+{
+    private readonly List<T> items;
+
+    public Box()
+    {
+        this.items = new List<T>();
+    }
+
+    public void Add(T elememnt)
+    {
+        this.items.Add(elememnt);
+    }
+
+    public T Remove()
+    {
+        var removeEl = this.items.Last();
+
+        items.RemoveAt(this.items.Count - 1);
+
+        return removeEl;
+    }
+
+    public int Count => this.items.Count();
+}
+
